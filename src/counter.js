b6847301ca233670035fccb1e1bab485b3997042
sidharth756 <sidharthrk756@gmail.com>
2026-03-30 12:22:04 +0530

    Added Hello World and count to +3

--- src/counter.js
+++ src/counter.js
@@ -4,6 +4,6 @@ export function setupCounter(element) {
     counter = count
     element.innerHTML = `Count is ${counter}`
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
+  element.addEventListener('click', () => setCounter(counter + 3))
   setCounter(0)
 }
